c04de0a3d47e6acfa7b2505beadaacfe203e3b06
rall <toto_min4016@naver.com>
2023-06-14 18:54:34 +0900

    draggable set

--- src/components/SortComp.vue
+++ src/components/SortComp.vue
@@ -1,30 +1,84 @@
 <template>
     <div>
-        <ul id="items" style="margin: 0 auto; width: 500px;">
-            <li style="width: 500px; height: 500px; background-color: blue;"></li>
-            <li style="width: 500px; height: 500px; background-color: red;"></li>
+        <draggable 
+            v-model="itemlist"
+            @start="drag=true" 
+            @end="drag=false"
+            :component-data="getComponentData()"
+            item-key="id">
+            <template #item="{element}">
+                <div>{{element.name}}</div>
+            </template>
+        </draggable>
+
+        <ul>
+            <li :key="item.id" :id="item.id" v-for="item in contlist" style="width: 300px; height: 300px; border: 1px solid; float: left;">
+                {{ item.name }}
+            </li>
         </ul>
     </div>
 </template>
 <script>
-import Sortable from 'sortablejs'
-//   import draggable from 'vuedraggable'
+// import Sortable from 'sortablejs'
+// import _ from 'lodash'
+  import draggable from 'vuedraggable'
   export default {
         components: {
-            // draggable,
+            draggable,
         },
         data() {
           return {
             drag: false,
+            itemlist: [
+                {
+                    id: 1,
+                    name: 'list1',
+                },
+                {
+                    id: 2,
+                    name: 'list2',
+                },
+                {
+                    id:3,
+                    name: 'list3',
+                }
+            ],
+            contlist: [
+                {
+                    id: 1,
+                    name: 'cont1',
+                    num: 0
+                },
+                {
+                    id:2,
+                    name: 'cont2',
+                    num: 1
+                },
+                {
+                    id:3,
+                    name: 'cont3',
+                    num: 2
+                }
+            ],
+            activeNames:''
           }            
         },
-        mounted() {
-            var el = document.getElementById('items');
-            new Sortable(el, {
-                swapThreshold: 0.78,
-                animation: 150
-            });
-        },
+        methods: {
+            handleChange() {
+            console.log('changed');
+            },
+            inputChanged(value) {
+                console.log(value);
+            },
+            getComponentData() {
+            return {
+                onChange: this.handleChange,
+                onInput: this.inputChanged,
+                wrap: true,
+                value: this.activeNames
+            };
+            }
+        }
     }
 </script>
 <style>
